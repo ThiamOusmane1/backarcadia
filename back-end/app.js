5d4ba889d4f48a62bceba03dac977dafcec90dae
thiam <thiam.o81@outlook.com>
2024-11-17 20:23:38 +0100

    test

--- back-end/app.js
+++ back-end/app.js
@@ -31,7 +31,7 @@ app.use(cors({
 
 // Middleware pour ajouter les en-têtes CORS
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8080', 'https://zoo-arcadia-front.vercel.app');
+    res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8080');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
